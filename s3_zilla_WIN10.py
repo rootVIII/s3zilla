016451e18891b1ab46e1df2ec34fab9f7c489959
rootVIII <colleyloyejames@gmail.com>
2019-05-12 15:25:36 -0400

    check if S3 buckets exist

--- s3_zilla_WIN10.py
+++ s3_zilla_WIN10.py
@@ -70,7 +70,7 @@ class S3Zilla:
         self.dropdown = StringVar()
         self.dropdown_data = self.populate_dropdown()
         if not self.dropdown_data:
-            self.dropdown_data = ['None Available']
+            self.dropdown_data = ['none available']
         self.deleted = False
         self.local_sel, self.s3_sel = ([] for i in range(2))
         self.title_label = Label(
