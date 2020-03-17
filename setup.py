f437bc8541b59292b788c8b462e15c60bec19a58
rootVIII <colleyloyejames@gmail.com>
2020-03-16 21:35:46 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="s3zilla",
-    version="1.0.0",
+    version="1.0.1",
     packages=["s3zilla"],
     package_data={
         's3zilla': ['s3zilla/*']
